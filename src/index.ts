35cb428fd13e9f2ed9438ebef4b150cb22f55cec
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-09-26 21:36:34 +0330

    class CharactersCollection added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 
+const charactersCollection = new CharactersCollection('hello');
+const sorter2 = new Sorter(charactersCollection);
+
 sorter.sort();
+sorter2.sort();
+
 console.log(numbersCollection.data);
+console.log(charactersCollection.data);
