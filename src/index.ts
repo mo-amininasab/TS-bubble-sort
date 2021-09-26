f89f5acfdcb224ad15a3be2f6a63790c6852a6fc
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-09-26 22:12:34 +0330

    inheritance and abstract class added and interface removed

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+numbersCollection.sort()
 
 const charactersCollection = new CharactersCollection('hello');
-const sorter2 = new Sorter(charactersCollection);
+charactersCollection.sort()
 
-sorter.sort();
-sorter2.sort();
+// sorter.sort();
+// sorter2.sort();
 
 console.log(numbersCollection.data);
 console.log(charactersCollection.data);
